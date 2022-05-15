66c408a2d1dcbe227b28ea4ca50f7cfd9a42e6b4
Thibault Arnal <thibaultarnal@hotmail.fr>
2022-05-15 16:22:43 +0000

    django.conf.urls to django.urls

--- guests/urls.py
+++ guests/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from guests.views import GuestListView, export_guests, invitation, rsvp_confirm, dashboard
 
 urlpatterns = [
-    url(r'^guests/$', GuestListView.as_view(), name='guest-list'),
-    url(r'^dashboard/$', dashboard, name='dashboard'),
-    url(r'^guests/export$', export_guests, name='export-guest-list'),
-    url(r'^invite/(?P<invite_id>[\w-]+)/$', invitation, name='invitation'),
-    url(r'^rsvp/confirm/(?P<invite_id>[\w-]+)/$', rsvp_confirm, name='rsvp-confirm'),
+    re_path(r'^guests/$', GuestListView.as_view(), name='guest-list'),
+    re_path(r'^dashboard/$', dashboard, name='dashboard'),
+    re_path(r'^guests/export$', export_guests, name='export-guest-list'),
+    re_path(r'^invite/(?P<invite_id>[\w-]+)/$', invitation, name='invitation'),
+    re_path(r'^rsvp/confirm/(?P<invite_id>[\w-]+)/$', rsvp_confirm, name='rsvp-confirm'),
 ]
