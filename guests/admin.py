bdbde0d7c7bf5f86dce60d3b0182556a6bde52c4
Thibault Arnal <thibaultarnal@hotmail.fr>
2022-07-29 08:25:23 +0000

    add export to CSV

--- guests/admin.py
+++ guests/admin.py
@@ -1,5 +1,28 @@
+import csv
+import codecs
+from django.http import HttpResponse
 from django.contrib import admin
 from .models import Guest, Party, Invitation
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        response.write(codecs.BOM_UTF8)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 
 class GuestInline(admin.TabularInline):
@@ -14,9 +37,10 @@ class PartyAdmin(admin.ModelAdmin):
     ]
 
 
-class GuestAdmin(admin.ModelAdmin):
+class GuestAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('first_name', 'last_name', 'is_attending', 'is_child', 'is_staying', 'vegetarian_option_selected')
     list_filter = ('is_attending', 'is_child', 'is_staying', 'vegetarian_option_selected')
+    actions = ['export_as_csv']
 
 
 class InvitationAdmin(admin.ModelAdmin):
